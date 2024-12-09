556ed1d8419eb1ca43da228cb9b549a74c931eea
thien-repo <thienpham0109@gmail.com>
2024-12-09 00:02:55 -0600

    test

---
+++ build/CMakeFiles/p.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/Thien/OneDrive/Desktop/gitrepos/p-lang/src/lexer.cpp" "CMakeFiles/p.dir/src/lexer.cpp.o" "gcc" "CMakeFiles/p.dir/src/lexer.cpp.o.d"
+  "/mnt/c/Users/Thien/OneDrive/Desktop/gitrepos/p-lang/src/p.cpp" "CMakeFiles/p.dir/src/p.cpp.o" "gcc" "CMakeFiles/p.dir/src/p.cpp.o.d"
+  "/mnt/c/Users/Thien/OneDrive/Desktop/gitrepos/p-lang/src/standard.cpp" "CMakeFiles/p.dir/src/standard.cpp.o" "gcc" "CMakeFiles/p.dir/src/standard.cpp.o.d"
+  "/mnt/c/Users/Thien/OneDrive/Desktop/gitrepos/p-lang/src/variable.cpp" "CMakeFiles/p.dir/src/variable.cpp.o" "gcc" "CMakeFiles/p.dir/src/variable.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
